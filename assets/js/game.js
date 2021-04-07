bcc0c11104d5830f84eb8a906064e1793a22b0fc
Seve Barrutia <sevebarrutia@me.com>
2021-04-07 10:49:48 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,12 +6,20 @@ var PlayerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames [0]);
+console.log(enemyNames [1]);
+console.log(enemyNames [2]);
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var fight = function(enemyName){
+    }
 
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
 
 // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -53,6 +61,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
     // if no (false), ask question again by running fight() again
     else {
-      fight();
+    
+fight();
     }
   }
